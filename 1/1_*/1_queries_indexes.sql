630eea9bd62803cf947f5de3bef997fb197fbd7a
Egorilla <114509561+Egori11a@users.noreply.github.com>
2025-04-18 23:37:20 +0300

    Update 1_queries_indexes.sql

--- 1/1_*/1_queries_indexes.sql
+++ 1/1_*/1_queries_indexes.sql
@@ -9,7 +9,14 @@ EXPLAIN ANALYZE
 SELECT * FROM reviews
 WHERE to_tsvector('russian', comment) @@ plainto_tsquery('russian', 'качественный товар');
 
--- Поиск по дате
+-- Поиск по дате без индекса
 EXPLAIN ANALYZE
 SELECT * FROM orders
 WHERE order_date >= '2024-01-01' AND order_date < '2025-01-01';
+
+-- Поиск по дате с индексом
+-- SET enable_seqscan = OFF;  -- Отключаем последовательное сканирование
+-- EXPLAIN ANALYZE
+-- SELECT * FROM orders
+-- WHERE order_date >= '2024-01-01' AND order_date < '2025-01-01';
+-- SET enable_seqscan = ON;   -- Включаем обратно (рекомендуется)
